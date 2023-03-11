4092feec105473b41f5b17da47f44c9669709b8a
GaDorrrr <gadsinoue@gmail.com>
2023-03-11 01:36:00 +0100

    Commit

--- Audio_Converter.py
+++ Audio_Converter.py
@@ -11,38 +11,56 @@ class AC(): #Audio Converter
     def __init__(self, FPS, FS, f) : #Résolution de l'analyse , fréquence d'échantillonage, audio pré-chargé
         self.FPS=FPS
         self.fs=FS
-        self.f=f #on prend la fréquence d'échantillonage de librosa pour commencer (22050 hz)
+        self.f=f
+        self.max_f=0.00001
 
-    def normalize(self,f): #normalise les amplitudes de l'audio
+    def normalize_f(self):
+        fft=np.fft.fft(self.f,len(self.f))
         max=0
-        for i in range(len(f)):
-            if f[i]>max:
-                max=f[i]
-
-        normalized=f/max
+        f_abs=np.abs(fft).real
+        for i in range(len(self.f)):
+            if f_abs[i]>max:
+                max=f_abs[i]
+        self.max_f=max
+    
+    def normalize(self,f): #normalise les amplitudes de l'audio       
+        normalized=f/self.max_f
         return(normalized)
 
-    def FFT(self,f): #on implementera notre propre algorithme FFT plus tard
-        fft=np.fft.fft(f,len(f))
+    def FFT(self,f,windowing): #on implementera notre propre algorithme FFT plus tard
+        if windowing :
+            l=[]
+            g=np.hanning(len(f))
+            for i in range(len(f)):
+                l.append(f[i]*g[i])
+            fft=np.fft.fft(l,len(l))
+        else :
+            fft=np.fft.fft(f,len(f))
         fft=np.abs(fft).real
-        fft=self.normalize(fft)
         return(fft)
     
-    def analyze(self): #partitionne l'audio en fonction des FPS qu'on a choisit afin de faire l'analyse de fourier sur chaque portion
+    def analyze(self,windowing): #partitionne l'audio en fonction des FPS qu'on a choisit afin de faire l'analyse de fourier sur chaque portion
         FFTS=[] 
         time=len(self.f)/self.fs
         image_count=floor(time*self.FPS)
 
         for i in range(image_count):
-            FFTS.append(self.FFT(self.f[floor(i*len(self.f)/image_count):floor((i+1)*len(self.f)/image_count)]))
-        
+            FFTS.append(self.FFT(self.f[floor(i*len(self.f)/image_count):floor((i+1)*len(self.f)/image_count)],windowing))
+        #on normalise l'ensemble des valeurs
+        max=np.amax(FFTS[0])
+        for i in range(1,len(FFTS)):
+            amax=np.amax(FFTS[i])
+            if amax>max :
+                max=amax
+        for i in range(len(FFTS)):
+            FFTS[i]=FFTS[i]/max
         return(FFTS)
     
     def visualize(self,f): #visualise le fichier audio sur une partie de la bande son
         f=self.normalize(f)
         Pxx=[i/self.fs for i in range(len(self.f))]
         fig,ax = plt.subplots()
-        plt.plot(Pxx[10000:20000],f[10000:20000], linewidth=2)
+        plt.plot(Pxx,f, linewidth=2)
         plt.ylabel('Amplitude')
         plt.xlabel('Temps(sec)')
         plt.show()
@@ -56,12 +74,13 @@ class AC(): #Audio Converter
         plt.xlabel('Fréquence(Hertz)')
         plt.show()
     
-    def image_generator(self,fmax): #génère une séquence d'image représentant l'évolution du spectre audio dans le temps
-        FFTS=self.analyze()
+    def image_generator(self,fmax,windowing): #génère une séquence d'image représentant l'évolution du spectre audio dans le temps
+        FFTS=self.analyze(windowing)
         Pxx=[j*self.fs/floor(self.fs/self.FPS) for j in range(floor(self.fs/self.FPS))]
         x=[num for num in Pxx if num<=fmax]
         for i in range(len(FFTS)):
             plt.plot(x,FFTS[i][:len(x)], linewidth=2)
+            plt.ylim([0,1])
             plt.ylabel('Magnitude')
             plt.xlabel('Fréquence(Hertz)')
             name="./Image_gen/"+str(i)+".png"
--- __Init_AC__.py
+++ __Init_AC__.py
@@ -1,10 +1,14 @@
 from Audio_Converter import*
 import librosa
+import numpy as np
 PATH="./Sound/Test.wav"
 
 f, fs = librosa.load(PATH,sr=44100)
-ac= AC(15,fs,f)
+ac= AC(15,fs,f-np.mean(f)) #retire mean(f) pour ne pas avoir de bruit constant en fond, peut être assimilé à un offset
 
-ac.image_generator(1000)
+ac.image_generator(4000,windowing=True)
 
 ac.images_to_video("./Image_gen",".png","Sequence",".mp4", PATH)
+
+
+
