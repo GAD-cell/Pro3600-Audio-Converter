a4b981328c5569d9f72f3c5cf461681789be5c03
GaDorrrr <gadsinoue@gmail.com>
2023-03-11 13:39:42 +0100

    commit

--- __Init_AC__.py
+++ __Init_AC__.py
@@ -5,7 +5,7 @@ PATH="./Sound/Tuning_fork.mp3"
 f, fs = librosa.load(PATH,sr=22050)
 ac= AC(30,fs,f-np.mean(f),window_size=0.25) #retire mean(f) pour ne pas avoir de bruit constant en fond, peut être assimilé à un offset
 
-ac.image_generator(1000,windowing=False)
+ac.image_generator(1000,windowing=True)
 
 ac.images_to_video("./Image_gen",".png","Sequence",".mp4", PATH)
 
