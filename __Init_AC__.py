48bcce65e7f735b26b2f6452c080e4e6a6991a0e
GaDorrrr <gadsinoue@gmail.com>
2023-03-16 01:06:33 +0100

    Menuet

--- __Init_AC__.py
+++ __Init_AC__.py
@@ -1,11 +1,11 @@
 from Audio_Converter import*
 import librosa
-PATH="./Sound/Test.wav"
+PATH="./Sound/Menuet_short.mp3"
 
 f, fs = librosa.load(PATH,sr=22050)
+tempo,beat_frames=librosa.beat.beat_track(y=f,sr=fs)
 ac= AC(30,fs,f-np.mean(f),window_size=0.25) #retire mean(f) pour ne pas avoir de bruit constant en fond, peut être assimilé à un offset
 
-
 ac.image_generator(4000,version=2,windowing=True) # fmax affiché, V1 ou V2 , algo de fenêtrage ou pas
-ac.images_to_video(image_folder_path="./Image_gen",extension=".png",video_name="Test",output_format=".mp4", audioclip=PATH)
+ac.images_to_video(image_folder_path="./Image_gen",extension=".png",video_name="Menuet",output_format=".mp4", audioclip=PATH)
 
