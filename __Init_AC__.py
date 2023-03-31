08b0f798e3dac8fc53f9ea384517c6ec3ecbe401
jgtzr <juliette.gantzer@telecom-sudparis.eu>
2023-03-31 19:08:03 +0200

    coucou

--- __Init_AC__.py
+++ __Init_AC__.py
@@ -10,3 +10,4 @@ ac= AC(30,fs,f-np.mean(f),window_size=1) #retire mean(f) pour ne pas avoir de br
 ac.image_generator(4000,version=2,windowing=True) # fmax affiché, V1 ou V2 , algo de fenêtrage ou pas
 ac.images_to_video(image_folder_path="./Image_gen",extension=".png",video_name="Test_superposition",output_format=".mp4", audioclip=PATH)
 
+hyvflut
